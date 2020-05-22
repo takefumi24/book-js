ca63c4af74804361e680db3135cf960455f0e968
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-22 19:55:55 +0900

    位置情報からデータを取得する

---
+++ practice/7-02_api/script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+//geolocation
+function success(pos) {
+  ajaxRequest(pos.coords.latitude, pos.coords.longitude);
+}
+
+function fail(error) {
+  alert('位置情報の取得に失敗しました。エラーコード：' + error.code)
+}
+navigator.geolocation.getCurrentPosition(success, fail);
+
+//データ取得
+function ajaxRequest(lat, long) {
+  const url = 'https://api.openweathermap.org/data/2.5/forecast';
+  const appId = '33473163a60f815ada0d36c4e19859a4'
+  $.ajax({
+    url: url,
+    data: {
+      appid: appId,
+      lat: lat,
+      lon: long,
+      units: 'metric',
+      lang:'ja'
+    }
+  })
+  .done(function (data) {
+    console.log(data);
+  })
+  .fail(function () {
+    console.log('$.ajax failed!')
+  })
+}
